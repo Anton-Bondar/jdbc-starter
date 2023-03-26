110eee25c99543412ab0b131073dd653255fa900
anbo <AntonBondar2013@gmail.com>
2023-03-26 21:45:27 +0300

    -add ConnectionManager for getting Connection -add properties for db connection

--- src/com/dmdev/starter/JdbcRunner.java
+++ src/com/dmdev/starter/JdbcRunner.java
@@ -1,10 +1,17 @@
 package com.dmdev.starter;
 
-import org.postgresql.Driver;
+import com.dmdev.starter.util.ConnectionManager;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.SQLException;
 
 public class JdbcRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        Class<Driver> driverClass = Driver.class;
+        try (Connection connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
-
 }
