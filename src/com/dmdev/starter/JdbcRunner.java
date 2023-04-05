d291c8c1b82e3782262f6541a7f40aa849448d5e
anbo <AntonBondar2013@gmail.com>
2023-04-05 18:14:06 +0300

    -add method bulk delete

--- src/com/dmdev/starter/JdbcRunner.java
+++ src/com/dmdev/starter/JdbcRunner.java
@@ -42,7 +42,6 @@ public class JdbcRunner {
                         }
                     }
                 }
-
             }
         }
         return result;
--- src/com/dmdev/starter/TransactionRunner.java
+++ src/com/dmdev/starter/TransactionRunner.java
@@ -5,24 +5,58 @@ import com.dmdev.starter.util.ConnectionManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class TransactionRunner {
 
+    private static final String DELETE_FLIGHT_SQL = "DELETE FROM flight WHERE id=%s";
+    private static final String DELETE_TICKET_BY_FLIGHT_ID_SQL = "DELETE FROM ticket WHERE flight_id=%s";
+
     public static void main(String[] args) throws SQLException {
         long flightId = 7;
-        var deleteFlightSql = "DELETE FROM flight WHERE id =?";
-        var deleteTicketSql = "DELETE FROM ticket WHERE flight_id=?";
+        //deleteWithException(flightId);
+        bulkDelete(flightId);
+    }
+
+    public static void bulkDelete(long flightId) throws SQLException {
+        Connection connection = null;
+        Statement statement = null;
+        try {
+            connection = ConnectionManager.open();
+            connection.setAutoCommit(false);
+
+            statement = connection.createStatement();
+            statement.addBatch(DELETE_TICKET_BY_FLIGHT_ID_SQL.formatted(flightId));
+            statement.addBatch(DELETE_FLIGHT_SQL.formatted(flightId));
+
+            statement.executeBatch();
+
+            connection.commit();
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+            if (statement != null) {
+                statement.close();
+            }
+        }
+    }
+
+    private static void deleteWithException(long flightId) throws SQLException {
         Connection connection = null;
         PreparedStatement deleteFlightStatement = null;
         PreparedStatement deleteTicketStatement = null;
         try {
             connection = ConnectionManager.open();
             connection.setAutoCommit(false);
-            deleteTicketStatement = connection.prepareStatement(deleteTicketSql);
-            deleteTicketStatement.setLong(1, flightId);
+            deleteTicketStatement = connection.prepareStatement(DELETE_TICKET_BY_FLIGHT_ID_SQL.formatted(flightId));
 
-            deleteFlightStatement = connection.prepareStatement(deleteFlightSql);
-            deleteFlightStatement.setLong(1, flightId);
+            deleteFlightStatement = connection.prepareStatement(DELETE_FLIGHT_SQL.formatted(flightId));
 
             deleteTicketStatement.executeUpdate();
             if (true) {
