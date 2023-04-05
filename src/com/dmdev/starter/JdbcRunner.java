4edfc486f538352f766dc6fdf9b42ac2de345866
anbo <AntonBondar2013@gmail.com>
2023-04-05 16:44:03 +0300

    -add method createInfo

--- src/com/dmdev/starter/JdbcRunner.java
+++ src/com/dmdev/starter/JdbcRunner.java
@@ -22,6 +22,7 @@ public class JdbcRunner {
         List<String> allTables = getAllTables(dbName);
         System.out.format("\nFlights between %s and %s are: %s", startDate, endDate, flights);
         System.out.format("\nDB %s has following tables are: %s", dbName, allTables);
+        System.out.format("\nNew info created with id %d ", createInfo("My info"));
     }
 
     public static List<String> getAllTables(String dbName) throws SQLException {
@@ -35,7 +36,7 @@ public class JdbcRunner {
                 while (schemas.next()) {
                     String schema = schemas.getString("TABLE_SCHEM");
                     if (schema.equals("public") && catalog.equals(dbName)) {
-                        ResultSet tables = metaData.getTables(catalog, schema, "%", new String[] {"TABLE"});
+                        ResultSet tables = metaData.getTables(catalog, schema, "%", new String[]{"TABLE"});
                         while (tables.next()) {
                             result.add(tables.getString("TABLE_NAME"));
                         }
@@ -46,47 +47,67 @@ public class JdbcRunner {
         }
         return result;
     }
-        private static List<Long> getFlightsBetween (LocalDateTime startDate, LocalDateTime endDate) throws SQLException
-        {
-            List<Long> result = new ArrayList<>();
-            String sql = """
-                    SELECT id 
-                    FROM flight
-                    WHERE departure_date BETWEEN ? AND ?
-                    """;
-            try (var connection = ConnectionManager.open();
-                 var preparedStatement = connection.prepareStatement(sql)) {
-                preparedStatement.setFetchSize(50);
-                preparedStatement.setQueryTimeout(10);
-                preparedStatement.setMaxRows(100);
 
-                preparedStatement.setTimestamp(1, Timestamp.valueOf(startDate));
-                preparedStatement.setTimestamp(2, Timestamp.valueOf(endDate));
+    private static List<Long> getFlightsBetween(LocalDateTime startDate, LocalDateTime endDate) throws SQLException {
+        List<Long> result = new ArrayList<>();
+        String sql = """
+                SELECT id 
+                FROM flight
+                WHERE departure_date BETWEEN ? AND ?
+                """;
+        try (var connection = ConnectionManager.open();
+             var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setFetchSize(50);
+            preparedStatement.setQueryTimeout(10);
+            preparedStatement.setMaxRows(100);
 
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    result.add(resultSet.getLong("id"));
-                }
-                return result;
-            }
-        }
+            preparedStatement.setTimestamp(1, Timestamp.valueOf(startDate));
+            preparedStatement.setTimestamp(2, Timestamp.valueOf(endDate));
 
-        private static List<Long> getTicketsByFlightId (Long flightId) throws SQLException {
-            String sql = """
-                    SELECT id 
-                    FROM ticket
-                    WHERE flight_id = ?
-                    """;
-            List<Long> result = new ArrayList<>();
-            try (var connection = ConnectionManager.open();
-                 var statement = connection.prepareStatement(sql)) {
-                statement.setLong(1, flightId);
-                var resultSet = statement.executeQuery();
-
-                while (resultSet.next()) {
-                    result.add(resultSet.getObject("id", Long.class)); // Null safe
-                }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                result.add(resultSet.getLong("id"));
             }
             return result;
         }
     }
+
+    private static List<Long> getTicketsByFlightId(Long flightId) throws SQLException {
+        String sql = """
+                SELECT id 
+                FROM ticket
+                WHERE flight_id = ?
+                """;
+        List<Long> result = new ArrayList<>();
+        try (var connection = ConnectionManager.open();
+             var statement = connection.prepareStatement(sql)) {
+            statement.setLong(1, flightId);
+            var resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                result.add(resultSet.getObject("id", Long.class)); // Null safe
+            }
+        }
+        return result;
+    }
+
+    public static int createInfo(String text) throws SQLException {
+        Integer result = null;
+        String sql = """
+                INSERT INTO info (data)
+                VALUES
+                ('%s')
+                """.formatted(text);
+        try (var connection = ConnectionManager.open();
+             var statment = connection.createStatement()) {
+
+            statment.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet generatedKeys = statment.getGeneratedKeys();
+
+            if (generatedKeys.next()) {
+                result = generatedKeys.getObject(1, Integer.class);
+            }
+        }
+        return result;
+    }
+}
